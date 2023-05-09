ff7a682ed109b6320c2b2f5c3f6bf23c125d60e1
shubhendu-ghosh-DS <shubhendughosh00@gmail.com>
2023-05-09 07:13:52 +0530

    T-2 | add translator

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 from flask import Flask, render_template, request
 import requests
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        from_lang = request.form['from_lang']
-        to_lang = request.form['to_lang']
+        src_lang = request.form['src_lang']
+        tgt_lang = request.form['tgt_lang']
         text = request.form['text']
-        translation = translate(from_lang, to_lang, text)
+        translation = translate(src_lang, tgt_lang, text)
         return render_template('home.html', translation=translation)
     else:
         return render_template('home.html')
+    
 
-def translate(from_lang, to_lang, text):
-    # Replace with your AI translation API endpoint
-    url = 'https://your-translation-service.com/translate'
-    payload = {'from_lang': from_lang, 'to_lang': to_lang, 'text': text}
-    response = requests.post(url, json=payload)
-    return response.json()['translation']
+tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
+
+model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
+
+def translate(tgt_lang, src_lang, text):
+    translator = pipeline(
+        "translation",
+        model=model,
+        tokenizer=tokenizer,
+        src_lang=src_lang,
+        tgt_lang=tgt_lang,
+    )
+
+    output = translator(text, max_length=400)
+    output = output[0]["translation_text"]
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True)
